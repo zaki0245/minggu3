4687471f4d288444cee609880fe7b207b36b8952
zaki0245 <muhammad.zaki0245@gmail.com>
2021-09-15 17:28:18 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+Metro City Template
+http://www.templatemo.com/tm-470-metro-city
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HOME</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/templatemo-style.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="tm-header">
+				<a href="index.html" class="tm-site-name">HOME</a>
+				<div class="mobile-menu-icon">
+	              <i class="fa fa-bars"></i>
+	            </div>
+				<nav class="tm-nav">
+					<ul>
+                        <li><a href="http://127.0.0.1:8000/profile" class="active">PROFILE</a></li>
+						<li><a href="http://127.0.0.1:8000/home">HOME</a></li>
+					</ul>
+				</nav>
+			</div>			
+		</div>		
+	</div>
+	<div class="tm-banner tm-bn-3">
+		<div class="container">
+			<div class="row">
+				<div class="tm-banner-text">
+					<div class="tm-banner-text-inner">
+						<h1 class="tm-banner-title">BERITA</h1>
+						
+					</div>	
+				</div>			
+			</div>			
+		</div>			
+	</div>	
+	<div class="container">
+		<div class="row">
+			<div class="boxes-container">
+				<div class="boxes-left">
+					<div class="box">
+						<div class="tm-block-1 tm-text-block content">
+							<p class="text-justify tm-gray-text">Laporan Satgas Covid-19 mencatat penambahan kasus positif di DKI hari ini sebanyak 306. DKI berada di urutan ketiga setelah Jawa Barat dengan 567 kasus positif, dan Jawa Timur 367 kasus positiF</p>
+						</div>	
+					</div>
+					<div class="box">
+						<img src="img/features-1.jpg" alt="Features" class="tm-block-1 content">	
+					</div>					
+					<div class="box">
+						<a href="#" class="content">
+							<div class="tm-block-1 tm-text-block tm-blue-box">
+								<i class="fa fa-align-left fa-3x"></i>
+								<h3 class="text-uppercase">Latest News</h3>
+							</div>	
+						</a>	
+					</div>
+					<div class="box">
+						<div class="tm-block-1 tm-text-block content">
+							<p class="text-justify tm-gray-text">Padahal sehari sebelumnya yakni Sabtu (11/9) DKI Jakarta ada di posisi enam enggan tambahan kasus positif sebanyak 268 kasus. Tiga hari sebelumnya, DKI Jakarta juga berhasil bertahan di posisi 5.</p>
+						</div>	
+					</div>	
+				</div>
+				<div class="boxes-right">
+					<div class="box col-lg-push-6 col-md-push-6 col-sm-push-6 col-xs-push-6">
+						<img src="img/features-2.jpg" alt="Features" class="tm-block-1 content">	
+					</div>					
+					<div class="box col-lg-pull-6 col-md-pull-6 col-sm-pull-6 col-xs-pull-6">
+						<div class="tm-block-1 tm-text-block content">
+							
+							<p class="text-justify tm-gray-text"><span class="tm-blue-text">Padahal sehari sebelumnya yakni Sabtu (11/9) DKI Jakarta ada di posisi enam enggan tambahan kasus positif sebanyak 268 kasus. Tiga hari sebelumnya, DKI Jakarta juga berhasil bertahan di posisi 5.</p>
+						</div>	
+					</div>					
+					<div class="box">
+						<a href="#" class="content">
+							<div class="tm-block-1 tm-text-block tm-red-box">
+								<i class="fa fa-comments-o fa-3x"></i>
+								<h3 class="text-uppercase">NEWS</h3>
+							</div>	
+						</a>			
+					</div>
+					<div class="box">
+						<div class="tm-block-1 tm-text-block content">
+							<p class="text-justify tm-gray-text">Rinciannya pada 10 September di posisi 5 dengan 261 kasus, kemudian 9 September di posisi lima dengan 308 kasus, dan 8 September di posisi lima meski catatan penambahan kasus hariannya 459 kasus.</p>
+						</div>	
+					</div>	
+				</div>
+			</div>		
+		</div>
+		<div class="row">
+			<div class="tm-block-2-container">				
+				<div class="tm-block-2 tm-block-order-2">
+					<a href="#">
+						<img src="img/features-4.jpg" alt="Features" class="img-responsive width-100-p">
+					</a>
+					<div class="tm-footer-block tm-blue-box tm-footer-text-container">
+						<h3 class="tm-footer-text-title">Design With Happiness</h3>
+					</div>
+				</div>
+				<div class="tm-block-2 tm-block-order-1">
+					<a href="#">
+						<div class="tm-position-relative">
+							<img src="img/features-3.jpg" alt="Features" class="img-responsive width-100-p">	
+							<div class="tm-block-2-text">
+								<p><span class="margin-right-20"><i class="fa fa-heart"></i> 72</span><i class="fa fa-comment"></i> 68</p>
+								<p class="text-uppercase">By zaki</p>
+							</div>		
+						</div>						
+					</a>
+					<p class="tm-footer-block tm-copyright-text">
+                    	Copyright &copy; 2084 Metro City 
+                    	| Design: <a rel="nofollow" href="http://www.templatemo.com/tm-470-metro-city" target="_parent">Metro City</a>
+                    </p>
+				</div>				
+			</div>				
+		</div>			  			  	
+	</div>
+	
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+	<script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+Metro City Template
+http://www.templatemo.com/tm-470-metro-city
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PROFILE</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/templatemo-style.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="tm-header">
+				<a href="index.html" class="tm-site-name">PROFILE</a>
+				<div class="mobile-menu-icon">
+	              <i class="fa fa-bars"></i>
+	            </div>
+				<nav class="tm-nav">
+					<ul>
+						<li><a href="http://127.0.0.1:8000/profile" class="active">PROFILE</a></li>
+						<li><a href="http://127.0.0.1:8000/home">HOME</a></li>
+					</ul>
+				</nav>
+			</div>			
+		</div>		
+	</div>
+	<div class="tm-banner tm-bn-1">
+		<div class="container">
+			<div class="row">
+				<div class="tm-banner-text">
+					<div class="tm-banner-text-inner">
+						<h1 class="tm-banner-title">NAMA : MUHAMMAD ZAKI</h1>
+					</div>	
+				</div>			
+			</div>			
+		</div>			
+	</div>	
+	<div class="container">
+		<div class="row">
+			<div class="tm-blocks-container">
+				<div class="tm-home-left">
+					<div class="tm-2-columns">						
+						<div class="tm-home-block-small tm-img-text tm-position-relative tm-white-box">
+							<div class="tm-social-icons-container">
+								<a href="#"><i class="tm-social-icon fa fa-facebook"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-twitter"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-rss"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-google-plus"></i></a>
+							</div>
+							<h2 class="tm-h2">TTL : JAKARTA, 18 SEPTEMBER 2002</h2>							
+						</div>
+						<div class="tm-home-block-small tm-position-relative">
+							<i class="fa fa-4x fa-caret-right tm-triangle-right"></i>
+							<img src="img/home-1.jpg" alt="image" class="tm-home-img-1">	
+						</div>										
+					</div>					
+					<div class="tm-home-block-big tm-position-relative">
+						<div class="tm-red-box tm-img-overlay tm-img-overlay-1">
+							<p>AGAMA : ISLAM</p>	
+						</div>						
+						<img src="img/home-3.jpg" alt="image" class="img-responsive">
+					</div>
+					<div class="tm-2-columns">						
+						<div class="tm-home-block-small tm-img-text tm-position-relative tm-white-box">
+							<div class="tm-social-icons-container">
+								<a href="#"><i class="tm-social-icon fa fa-facebook"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-twitter"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-rss"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-google-plus"></i></a>
+							</div>
+							<h2 class="tm-h2">ALAMAT : KAVLING TEGAL PERINTIS, BEKASI UTARA</h2>
+						</div>				
+						<div class="tm-home-block-small tm-position-relative">
+							<img src="img/home-5.jpg" alt="image" class="tm-home-img-1">		
+						</div>						
+					</div>
+				</div>			
+				<div class="tm-home-right">
+					<div class="tm-home-block-big tm-position-relative tm-home-img-big-2-container">
+						<div class="tm-blue-box tm-img-overlay tm-img-overlay-2">
+							<p>NOMOR : 085811252996</p>	
+						</div>						
+						<img src="img/home-2.jpg" alt="image" class="img-responsive">
+					</div>
+					<div class="tm-home-block-big tm-2-columns-tall">
+						<div class="col tm-intro-container tm-position-relative tm-white-box">
+							<div class="tm-social-icons-container">
+								<a href="#"><i class="tm-social-icon fa fa-facebook"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-twitter"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-rss"></i></a>
+								<a href="#"><i class="tm-social-icon fa fa-google-plus"></i></a>
+							</div>
+							<h2 class="tm-h2 tm-intro-title">CITA CITA : PROGRAMER</h2>			
+						</div>
+						<div class="col tm-2-rows tm-2-rows-2">
+							<div class="tm-home-block-small tm-home-img-1-container">
+								<img src="img/home-4.jpg" alt="image" class="tm-home-img-1">	
+							</div>							
+							<div class="tm-home-block-small padding-30 tm-dark-box tm-dark-box-img-text">
+								<h2 class="tm-h2 tm-dark-gray-text">HOBI : SEPAKBOLA</h2>
+						    </div>	
+						</div>						
+					</div>					
+				</div>
+			</div>			
+		</div>		
+		<div class="row">
+			<div class="tm-block-2-container">			
+				
+				<div class="tm-block-2 tm-block-order-2">
+					<div class="tm-footer-block tm-blue-box tm-footer-text-container">
+						<h3 class="tm-footer-text-title">Design With Happiness</h3>
+					</div>
+				</div>	
+				<div class="tm-block-2 tm-block-order-1">
+					<p class="tm-footer-block tm-copyright-text">
+                    	Copyright &copy; 2084 Metro City 
+                    	| Design: <a rel="nofollow" href="http://www.templatemo.com/tm-470-metro-city" target="_parent">Metro City</a>
+                    </p>
+				</div>			
+			</div>				
+		</div>			  			  	
+	</div>
+	
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+	<script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+</body>
+</html>
